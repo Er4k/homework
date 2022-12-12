470784de329455628b46108a7e7b85de91e7ee52
Er4k <119675410+Er4k@users.noreply.github.com>
2022-12-12 22:50:07 +0100

    Delete top_df_remote.sh

--- top_df_remote.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-scp top_df_local.sh dev@192.168.56.123:/home/dev/code
-
-ssh dev@192.168.56.102 "sh /home/dev/code/top_df_local.sh"
-
-scp -r dev@192.168.56.123:/home/dev/code/ /home/dev/code
